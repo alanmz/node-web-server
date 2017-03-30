32ec2cfad4819974a6fad089fdd16d4f9d301451
alanmz <alan_mz@outlook.com>
2017-03-29 20:21:54 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express= require('express');
+const hbs=require('hbs');
+var app=express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    console.log(now, req.method, req.url);
+    next();
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return  new Date().getFullYear()
+});
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        welcomeMessage:'hi world',
+        pageTitle:'home',
+       
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'about',
+       
+    });
+});
+
+app.listen(3000);
